02597f62a50ed0e1f276a20885037640e7cd00f9
JoMinsu <eovywjr1@naver.com>
2023-01-25 12:02:59 +0900

    3week_homework2

--- app.py
+++ app.py
@@ -2,13 +2,20 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/id/<int:id>', methods=['GET'])
-def get(id):
-    return {"message": ""}
+@app.route('/id/<int:id>', methods = ['GET'])
+def GET_API(id):
+    if id >= 5000 : return '{\'result\' : true}'
+    else : return '{\'result\' : false}'
+    
+@app.route('/id', methods = ['POST'])
+def POST_API():
+    params = request.get_json()
+    return jsonify(params['name'])
+    
 
-@app.route('/id', methods=['POST'])
-def post():
-    return {'name': ''}
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
